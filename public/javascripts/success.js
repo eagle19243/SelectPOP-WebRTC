3fa7e889173e571f032705afd8a6532091b265d3
eagle19243 <eagle19243@gmail.com>
2016-11-20 16:45:06 +0100

    First Commit

---
+++ public/javascripts/success.js
@@ -0,0 +1,7 @@
+/**
+ * Created by admin on 20/11/16.
+ */
+function onGenerateLink() {
+    console.log('onGenerateLink()');
+    $('.link').value('http://dev.selectpop.com/');
+}
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
-router.get("/a", function(req, res) {
-    var name = req.query.myName;
-    res.render("param", {
-        name : name
-    });
+router.post('/', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    if (username == 'a' && password == 'b') {
+        res.render('success');
+    } else {
+        res.render('fail');
+    }
 });
 
 module.exports = router;
