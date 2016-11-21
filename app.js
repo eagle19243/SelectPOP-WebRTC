99fba7c5827bba1eb533834b5c1cf9edd51d1ed4
eagle19243 <eagle19243@gmail.com>
2016-11-21 20:22:03 +0100

    First Commit

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
 var index = require('./routes/index');
-var user = require('./routes/user');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -22,10 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'secret'}));
 
 app.use('/', index);
-app.use('/user', user);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
