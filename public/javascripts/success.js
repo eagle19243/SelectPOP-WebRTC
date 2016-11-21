4b8425ff2f7fa4bb444e66d48b6838a863e1fc21
eagle19243 <eagle19243@gmail.com>
2016-11-21 22:13:32 +0100

    First Commit

---
+++ public/javascripts/success.js
@@ -0,0 +1,32 @@
+/**
+ * Created by admin on 20/11/16.
+ */
+
+$(document).ready(function () {
+    var username;
+
+    $.post('/user/getUsername', function(result) {
+        username = result;
+    })
+
+    /*--------------Generate Link button Handler-------------*/
+    $('.generateLink').click(function () {
+        $('.link').val('http://dev.selectpop.com/');
+    });
+
+    /*--------------MessageBox KeyDown Handler-------------*/
+    $('.msgBox').keyup(function (event) {
+        var timeString = new Date().toLocaleTimeString();
+
+        if (event.which == 13) {
+            $('.chatBox').append(
+                '<div class = "msgln">' +
+                '(' + timeString  + ') <b>' + username + '<b>:' + $('.msgBox').val() +
+                '</div>');
+            console.log($('.chatBox').html());
+            $('.msgBox').val('');
+            event.preventDefault();
+        }
+    });
+
+});
