a982583b4549e91781f978870e2b2b96dc5db1d4
eagle19243 <eagle19243@gmail.com>
2016-11-21 18:16:15 +0100

    added some features. - videoview (getUserMedia()) - user1 page - user2 page - login page

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,18 +1,17 @@
 /**
  * Created by admin on 20/11/16.
  */
-
 $(document).ready(function () {
     if (hasUserMedia()) {
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
             || navigator.mozGetUserMedia;
 
-        //enabling video and audio channels
         navigator.getUserMedia({ video: true, audio: true }, function (stream) {
             var video = document.querySelector('video');
 
-            //inserting our stream to the video tag
             video.src = window.URL.createObjectURL(stream);
+
+            console.log(stream);
         }, function (err) {});
     } else {
         alert("WebRTC is not supported");
