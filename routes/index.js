92ced6275d60f76b868e6a393b0a76c1a04bcb94
eagle19243 <eagle19243@gmail.com>
2016-11-21 20:30:37 +0100

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var title = 'SelectPOP Demo Project';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: title });
 });
 
 module.exports = router;
