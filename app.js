cb5d245e45cecb3fcddd505a30bf8842c75b86fd
eagle19243 <eagle19243@gmail.com>
2016-11-21 22:12:43 +0100

    First Commit

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var user = require('./routes/user');
 
 var app = express();
 
@@ -21,9 +22,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'secret'}));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var title = 'SelectPOP Demo Project';
+var session;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: title });
+    session = req.session;
+    if (session.username == 'a' && session.password == 'a') {
+        res.render('success');
+    } else {
+        res.render('index');
+    }
+});
+
+router.post('/', function(req, res) {
+    session = req.session;
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+    if (username == 'a' && password == 'a') {
+        session.username = username;
+        session.password = password;
+        res.render('success');
+    } else if (username = 'b'&& password == 'b') {
+        session.username = username;
+        session.password = password;
+        res.render('client');
+    } else {
+        res.render('fail');
+    }
 });
 
 module.exports = router;
