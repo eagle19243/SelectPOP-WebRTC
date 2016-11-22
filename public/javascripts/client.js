111ee36f4bbbbdb9fae22cd322acdc96fccb4450
eagle19243 <eagle19243@gmail.com>
2016-11-22 07:37:11 +0100

    First Commit

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,7 +1,7 @@
 /**
  * Created by admin on 20/11/16.
  */
-var url = 'ws://selectpop.herokuapp.com:3000';
+var url = 'ws://selectpop.herokuapp.com/';
 var connection = io.connect(url);
 var name = '';
 
